4d2b9cc307cb40ec08d3f8be7ac0f0d893aa4a77
Mehmet Doğukan <m.dogukansevinc@gmail.com>
2019-03-21 19:55:36 +0300

    Add files via upload

---
+++ LaunchScreenApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaunchScreenApp
+//
+//  Created by Mehmet Doğukan Sevinç on 21.03.2019.
+//  Copyright © 2019 Mehmet Doğukan Sevinç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
